f0f2f85d3f2078f4b0391e79ebfa72a5b85ac603
Miguel Angel <me@devmiguelangel.com>
2019-11-19 10:10:56 -0400

    schema and types

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 'use strict'
 
+const { graphql, buildSchema } = require('graphql');
+
+// Schema definition
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// Query execution
+graphql(schema, '{ hello }').then(data => {
+  console.log(data)
+});
